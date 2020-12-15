2d99f16ab24c6c2ab988c9fc45d29740f8aed5fe
Suslyak <sergey.suslyak@mail.ru>
2020-12-16 02:54:36 +0300

    доделан подвал + некоторые правки в других блоках; добавлены переменные цвета, доп. брейк-поинта для десктопа; добавлены webp

---
+++ build/js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+(function () {
+  var pageHeader = document.querySelector('.page-header');
+  var headerMenuToggle = document.querySelector('.page-header__menu-toggle');
+
+  pageHeader.classList.remove('page-header--nojs');
+  headerMenuToggle.classList.remove('page-header--nojs');
+})();
