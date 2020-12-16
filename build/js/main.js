a3f0f5ed24f130e1c3886bcc71e28cbd43eefb82
Suslyak <sergey.suslyak@mail.ru>
2020-12-17 02:50:57 +0300

    блок о компании; добавлены некоторые изображенния; добавлены состояния для промо-кнопки и логотипа; добавлен полифил для svg в ie11; некоторые подгонки под ie11

--- build/js/main.js
+++ build/js/main.js
@@ -2,7 +2,15 @@
 (function () {
   var pageHeader = document.querySelector('.page-header');
   var headerMenuToggle = document.querySelector('.page-header__menu-toggle');
+  var aboutUs = document.querySelector('.about-us');
+  var textToCut = aboutUs.querySelector('.about-us__text');
 
   pageHeader.classList.remove('page-header--nojs');
   headerMenuToggle.classList.remove('page-header--nojs');
+
+  var getContent = function (element) {
+    return element.innerText || element.textContent;
+  };
+
+  getContent(textToCut);
 })();
